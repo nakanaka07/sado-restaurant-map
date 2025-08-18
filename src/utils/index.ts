13df8530639e42e4c6960100b11720672d0eb727
Sado Restaurant Map Developer <nakaguchi07@gmail.com>
2025-08-18 20:15:38 +0900

    feat: ユーティリティモジュールの統一エクスポートを追加

---
+++ src/utils/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Utils モジュールの統一エクスポート
+ *
+ * @fileoverview 各ユーティリティモジュールをまとめてエクスポートする
+ * @version 1.0.0
+ */
+
+// Google Analytics関連
+export {
+  autoFixGA, checkGAStatus, debugGA, initGA, runGADiagnostics,
+  sendTestEvents, trackEvent, trackFilter,
+  trackMapInteraction, trackPageView, trackPWAUsage, trackRestaurantClick,
+  trackSearch
+} from './analytics';
+
+// 地区判定関連
+export {
+  getAllDistricts, getDistrictFromAddress, isValidDistrict, normalizeDistrict
+} from './districtUtils';
+
+// バリデーション関連
+export {
+  createValidationError, isArray, isCuisineType, isLatLngLiteral, isNumber, isObject, isPriceRange, isRestaurant, isRestaurantArray, isSadoDistrict, isString, isValidApiKey, isValidSearchQuery, sanitizeInput, validateRestaurant, type ValidationError
+} from './lightValidation';
+
+// セキュリティ関連
+export {
+  apiRateLimiter, checkSecurityHeaders, escapeHtml, generateCSRFToken, generateNonce,
+  getSecureEnvVar, isSecureUrl,
+  maskApiKey, safeJsonParse, sanitizeUserInput, searchRateLimiter, secureFetch, SecureStorage, stripHtml, validateApiKey
+} from './securityUtils';
